6ecb6906bd5f56b9eed4821ea1de84e91feb9db2
Braden <bradenoptimus@gmail.com>
2023-12-01 23:58:33 -0700

    I SWEAR I DID STUFF GITHUB

--- main.cpp
+++ main.cpp
@@ -2,14 +2,8 @@
 #include "mytime.h"
 
 int main(int argc, char ** argv) {
-    MyTime test = MyTime(1162494284);
-    std::cout << "Year: " << test.getYear() << std::endl;
-    std::cout << "Month: " << test.getMonthWord() << std::endl;
-    std::cout << "Day: " << test.getDay() << std::endl;
-    std::cout << "Hour: " << test.getHour() << std::endl;
-    std::cout << "Quarter: " << test.getQuarter() << std::endl;
-    std::cout << "Fiver: " << test.getFiver() << std::endl;
-    std::cout << "TZ: " << test.getTZ() << std::endl;
-    std::cout << "1162494284 vs. " << test.getRaw() << std::endl;
+    MyTime test = MyTime(4,20,69, 16, 20);
+    std::cout << test << std::endl;
+    std::cout << "Raw: " << test.getRaw() << std::endl;
     return 0;
 }
--- mytime.h
+++ mytime.h
@@ -2,20 +2,47 @@
 #define MYTIME
 
 #include <string>
+#include <iostream>
 
 class MyTime {
 private:
     unsigned int time;
 public:
+    // Constructor from raw int
     MyTime(unsigned int t): time(t) {}
+
+    // Default constructor
+    MyTime(): time(1162494284) {}
+
+    // mm-dd-yyyy type constructor
     MyTime(int month, int day, int year) {
-        
+        this->time = year;
+        this->time <<= 4;
+        this->time += month;
+        this->time <<= 5;
+        this->time += day;
+        this->time <<= 15;
     }
+
+    // mm-dd-yy hh:mm type constructor
     MyTime(int month, int day, int year, int hour, int minute) {
-        
+        this->time = year;
+        this->time <<= 4;
+        this->time += month;
+        this->time <<= 5;
+        this->time += day;
+        this->time <<= 5;
+        this->time += hour;
+        this->time <<= 2;
+        this->time += (minute / 15);
+        this->time <<= 2;
+        this->time += ((minute % 15)  / 5);
+        this->time <<= 6;
     }
+
+    // mm-dd-yy hh:mm tz type constructor 
     MyTime(int month, int day, int year, int hour, int minute, std::string tz) {
-        
+        // TODO: Make this work ---------------------------------------------------------------------
     }
 
     unsigned int getRaw() {
@@ -66,6 +93,11 @@ public:
     }
 
     int getHour() {
+        unsigned int temp = (this->time & 0x00007C00) >> 10;
+        return temp % 12 == 0 ? 12 : temp % 12;
+    }
+
+    int getMilHour() {
         return (this->time & 0x00007C00) >> 10;
     }
 
@@ -77,44 +109,64 @@ public:
         return (this->time & 0x000000C0) >> 6;
     }
 
+    int getMinutes() {
+        return (this->getQuarter()*15) + (this->getFiver() * 5);
+    }
+
     int getTZ() {
+        // TODO: Fix this ---------------------------------------------------------------------  
         return this->time & 0x0000003F;
     }
 
     void setYear() {
-        
+        // TODO: Make this work  --------------------------------------------------------------
     }
 
     void setMonth() {
-        
+        // TODO: Make this work  --------------------------------------------------------------
     }
 
     void setDay() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     void setHour() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     void setQuarter() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     void setFiver() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     void setMinutes() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     void setTZ() {
-        
+        // TODO: Make this work  --------------------------------------------------------------   
     }
 
     std::string toString() {
+        return std::to_string(this->getMonth()) + "-" + \
+        std::to_string(this->getDay()) +  "-" + \
+        std::to_string(this->getYear()) + " " + \
+        std::to_string(this->getHour()) + ":" + \
+        std::to_string(this->getMinutes()) + " " + \
+        (this->getMilHour() > 12 ? "PM" : "AM") + " " + \
+        std::to_string(this->getTZ());
+    }
+
+    std::string getMilitary() {
         return "lolz";
+    }
+
+    friend std::ostream & operator << (std::ostream& os, MyTime& f) {
+        os << f.toString();
+        return os;
     }
 };
 
